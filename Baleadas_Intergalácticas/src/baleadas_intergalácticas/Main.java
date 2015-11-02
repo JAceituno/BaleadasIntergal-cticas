4ce2bd97fb870b783cb59036fc1ecaf68b34ef6d
Ricardo Galdamez <ricardo.j.galdamez@gmail.com>
2015-11-02 17:59:04 -0600

    creacion de algunas clases

--- Baleadas_Intergalácticas/src/baleadas_intergalácticas/Main.java
+++ Baleadas_Intergalácticas/src/baleadas_intergalácticas/Main.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package baleadas_intergalácticas;
 
-/**
- *
- * @author rick
- */
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Main temporal para organizacion de los objetos
+        // TDA cola para la lista de ordenes pendientes
     }
     
 }
