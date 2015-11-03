d4e89c78e0c4811e457ffa1a18d20364bc27dac2
Ricardo Galdamez <ricardo.j.galdamez@gmail.com>
2015-11-03 12:28:17 -0600

    Creacion de más clases y organización general de datos dentro de las clases

--- Baleadas_Intergalácticas/src/baleadas_intergalácticas/Ingrediente.java
+++ Baleadas_Intergalácticas/src/baleadas_intergalácticas/Ingrediente.java
@@ -13,5 +13,28 @@ public class Ingrediente {
     
     private String nombre;
     private String descripción;
+
+    public Ingrediente(String nombre, String descripción) {
+        this.nombre = nombre;
+        this.descripción = descripción;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripción() {
+        return descripción;
+    }
+
+    public void setDescripción(String descripción) {
+        this.descripción = descripción;
+    }
+    
+    
     
 }
---
+++ Baleadas_Intergalácticas/src/baleadas_intergalácticas/Orden.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baleadas_intergalácticas;
+
+/**
+ *
+ * @author rick
+ */
+public class Orden {
+    
+    private int numOrden;
+    private String nombreCliente;
+    // Lista de Platos pedidas TDA lista
+    private double precioTotal;
+    private int preparacion;
+}
